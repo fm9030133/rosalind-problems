8c57cc9072d838c95a5d75e432474aac5ae6ada3
Framora <108380690+fm9030133@users.noreply.github.com>
2023-01-26 15:08:38 -0800

    Implement dna_str.replace('T', 'U')

---
+++ 1_RNA.py
@@ -0,0 +1,16 @@
+'''
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is 
+formed by replacing all occurrences of 'T' in t with 'U' in u
+
+Given: A DNA string t having length at most 1000 nt.
+Return: The transcribed RNA string of t
+
+Sample Dataset: GATGGAACTTGACTACGTAAATT
+Sample Output: GAUGGAACUUGACUACGUAAAUU
+'''
+
+dna_str = 'GATGGAACTTGACTACGTAAATT'
+rna = dna_str.replace('T', 'U')
+print(rna)
